7c814eec30231b9bd0d6460f6ee299eea0f298a7
Muhammed Talha Korkmaz <kriptovekediler@gmail.com>
2024-12-22 14:45:06 +0300

    fix:small fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="agentipy",
-    version="1.2.2",
+    version="1.2.3",
     author="Utilify",
     author_email="hello@getutilify.com",
     description="A Python toolkit for Solana agents",
